756b0a4b35f05801ce645473344137686eb9c55a
Soyon Lee <slee550@myseneca.ca>
2023-04-10 11:42:50 -0400

    ready to be live

---
+++ js/app.js
@@ -0,0 +1,103 @@
+//carousel
+const slides = document.querySelectorAll(".slide-fade");
+const dots = document.querySelectorAll(".dot");
+
+document.addEventListener("DOMContentLoaded", function () {
+  dots[0].addEventListener("click", function () {
+    slides[0].hidden = false;
+    slides[1].hidden = true;
+    slides[2].hidden = true;
+    slides[3].hidden = true;
+  });
+  dots[1].addEventListener("click", function () {
+    slides[1].hidden = false;
+    slides[0].hidden = true;
+    slides[2].hidden = true;
+    slides[3].hidden = true;
+  });
+  dots[2].addEventListener("click", function () {
+    slides[2].hidden = false;
+    slides[0].hidden = true;
+    slides[1].hidden = true;
+    slides[3].hidden = true;
+  });
+  dots[3].addEventListener("click", function () {
+    slides[3].hidden = false;
+    slides[0].hidden = true;
+    slides[1].hidden = true;
+    slides[2].hidden = true;
+  });
+});
+
+document.addEventListener("DOMContentLoaded", createCard);
+
+//card generation
+const cards = document.querySelector("#cards");
+const reviews = window.reviews;
+
+function createCard() {
+  reviews.forEach(function (review) {
+    let card = document.createElement("section");
+    let cardImg = document.createElement("img");
+    let cardName = document.createElement("p");
+    let cardDate = document.createElement("p");
+    let cardRate = document.createElement("div");
+    let cardReview = document.createElement("p");
+    //let star = document.createElement("span");
+
+    card.setAttribute("class", "card");
+    cardImg.setAttribute("src", `${review.imgSrc}`);
+    cardName.setAttribute("class", "card-name");
+    cardName.innerText = `${review.username}`;
+    cardDate.setAttribute("class", "card-date");
+    cardDate.innerHTML = `<time datetime="${review.date}">${review.date}</time>`;
+    cardRate.setAttribute("class", "card-rating");
+    cardReview.setAttribute("class", "card-review");
+    cardReview.innerText = `"${review.reviewText}"`;
+    //star.setAttribute("class", "material-symbols-outlined");
+    //star.innerText = " grade ";
+
+    cards.appendChild(card);
+    card.appendChild(cardImg);
+    card.appendChild(cardName);
+    card.appendChild(cardDate);
+    card.appendChild(cardRate);
+    for (let i = 0; i < review.rating; i++) {
+      const star = document.createElement("span");
+      star.setAttribute("class", "material-symbols-outlined");
+      star.innerText = " grade ";
+      cardRate.appendChild(star);
+    }
+    card.appendChild(cardReview);
+  });
+}
+
+//review creation
+const reviewBtn = document.querySelector("#create-review");
+
+document.addEventListener("DOMContentLoaded", function () {
+  reviewBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const username = document.querySelector("#username").value;
+    const date = document.querySelector("#date").value;
+    const rating = document.querySelector("#rating").value;
+    const review = document.querySelector("#review-text").value;
+
+    console.log("click");
+    console.log(username, date, rating, review);
+
+    let obj = {};
+    obj.username = username;
+    obj.date = date;
+    obj.rating = rating;
+    obj.reviewText = review;
+    obj.imgSrc = "./media/default_face.jpg";
+
+    reviews.push(obj);
+
+    //debugger;
+    cards.innerHTML = "";
+    createCard();
+  });
+});
---
+++ js/data.js
@@ -0,0 +1,37 @@
+window.reviews = [
+  {
+    username: "Charlie",
+    date: "2023-04-23",
+    rating: 5,
+    reviewText: "I never get over this game!",
+    imgSrc: "./media/face1.jpg",
+  },
+  {
+    username: "Micheal",
+    date: "2022-08-11",
+    rating: 4,
+    reviewText: "Best card game ever!",
+    imgSrc: "./media/face2.jpg",
+  },
+  {
+    username: "Frank",
+    date: "2020-03-02",
+    rating: 5,
+    reviewText: "With a pack of UNO, you'll be famous in any party!",
+    imgSrc: "./media/face3.jpg",
+  },
+  {
+    username: "Deborah",
+    date: "2015-12-15",
+    rating: 1,
+    reviewText: "No hard feeling for the game, but I was robbed!",
+    imgSrc: "./media/face4.jpg",
+  },
+  {
+    username: "Kristen",
+    date: "2019-04-08",
+    rating: 5,
+    reviewText: "It gave me a good laugh even I lost brutally!",
+    imgSrc: "./media/face5.jpg",
+  },
+];
